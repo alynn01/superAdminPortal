48793847f363c6a6a0811e4a4a0d575c18ee77d2
alynn01 <127193049+alynn01@users.noreply.github.com>
2023-08-14 16:47:23 +0100

    removed commented code
    removed commented code

--- cypress/e2e/Settlement and Treasury/settlement.cy.js
+++ cypress/e2e/Settlement and Treasury/settlement.cy.js
@@ -13,27 +13,27 @@ describe("Settlement and Treasury", () => {
     settlementPage.accessSettlementPage();
   });
 
-  // it("Test that the user is able to view emoney balance", () => {
-  //   settlementPage.accessEMoneyBalance();
-  // });
-  // it("Test that the user is able to view company balance", () => {
-  //   settlementPage.accessCompanyBalance();
-  // });
-  // it("Test that user can filter the report based on transaction  type", () => {
-  //   settlementPage.filterByDebit();
-  // });
-  // it("Test that the user can filter the report based on company name", () => {
-  //   settlementPage.filterByCompany();
-  // });
-  // it("Test that the user can filter the report based on amount", () => {
-  //   settlementPage.filterByAmount();
-  // });
-  // it("Test that user can filter by date", () => {
-  //   settlementPage.filterByDate();
-  // });
-  // it("Test that the user is able to search the report using the search field", () => {
-  //   settlementPage.searchReport();
-  // });
+  it("Test that the user is able to view emoney balance", () => {
+    settlementPage.accessEMoneyBalance();
+  });
+  it("Test that the user is able to view company balance", () => {
+    settlementPage.accessCompanyBalance();
+  });
+  it("Test that user can filter the report based on transaction  type", () => {
+    settlementPage.filterByDebit();
+  });
+  it("Test that the user can filter the report based on company name", () => {
+    settlementPage.filterByCompany();
+  });
+  it("Test that the user can filter the report based on amount", () => {
+    settlementPage.filterByAmount();
+  });
+  it("Test that user can filter by date", () => {
+    settlementPage.filterByDate();
+  });
+  it("Test that the user is able to search the report using the search field", () => {
+    settlementPage.searchReport();
+  });
   it("Test that the user is able to complete eMoney top up", () => {
       settlementPage.eMoneyTopup();
       settlementPage.uploadInvalidFile();
